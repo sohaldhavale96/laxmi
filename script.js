5bab8349b7ad166d49932895945ad25fe5fca213
sohaldhavale96 <sohaldhavale96@gmail.com>
2024-07-04 13:12:57 +0530

    about section done

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+gsap.registerPlugin(ScrollTrigger);
 const tl = gsap.timeline()
 const strips = document.querySelectorAll('.strip')
 const navElements = document.querySelectorAll('.nav-elems')
@@ -71,20 +72,82 @@ tl.from(rightContenth2,{
     opacity:0,
 })
 
-const aboutHead = document.getElementById('AboutUSHEAD')
-const aboutSectionL = document.querySelectorAll('.abtleft');
-const aboutSectionR = document.querySelectorAll('.abtright');
-tl.from(aboutHead,{
-    y:"30px",
-    opacity:0
-})
-tl.from(aboutSectionL,{
-    x:"-900px",
-    stagger:0.2,
-    opacity:0
-})
-tl.from(aboutSectionR,{
+// const aboutHead = document.getElementById('AboutUSHEAD')
+// const aboutSectionL = document.querySelectorAll('.abtleft');
+// const aboutSectionR = document.querySelectorAll('.abtright');
+// tl.from(aboutHead,{
+//     y:"30px",
+//     opacity:0
+// })
+// tl.from(aboutSectionL,{
+//     x:"-900px",
+//     stagger:0.2,
+//     opacity:0
+// })
+// tl.from(aboutSectionR,{
+//     x:"900px",
+//     stagger:0.2,
+//     opacity:0
+// })
+
+
+const card1 = document.getElementById('absec1')
+tl.from(card1,{
     x:"900px",
-    stagger:0.2,
-    opacity:0
+    opacity:0,
+    scrollTrigger:{
+        trigger: '#about',
+        start: '0% 50%', 
+        end: '15% 50%',
+        markers:true,
+        scrub: 2
+    }
+})
+const card2 = document.getElementById('absec2')
+tl.from(card2,{
+    x:"-900px",
+    opacity:0,
+    scrollTrigger:{
+        trigger: '#about',
+        start: '15% 50%', 
+        end: '30% 50%',
+        markers:true,
+        scrub: 2
+    }
+})
+const card3 = document.getElementById('absec3')
+tl.from(card3,{
+    x:"900px",
+    opacity:0,
+    scrollTrigger:{
+        trigger: '#about',
+        start: '30% 50%', 
+        end: '45% 50%',
+        markers:true,
+        scrub: 2
+    }
+})
+const card4 = document.getElementById('absec4')
+tl.from(card4,{
+    x:"-900px",
+    opacity:0,
+    scrollTrigger:{
+        trigger: '#about',
+        start: '45% 50%', 
+        end: '70% 50%',
+        markers:true,
+        scrub: 2
+    }
+})
+const card5 = document.getElementById('absec5')
+tl.from(card5,{
+    x:"900px",
+    opacity:0,
+    scrollTrigger:{
+        trigger: '#about',
+        start: '70% 50%', 
+        end: '85% 50%',
+        markers:true,
+        scrub: 2
+    }
 })
